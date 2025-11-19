e7029fe8ede43effca327966819ce978da057b43
VOVAN1980 <reker198079@gmail.com>
2025-11-19 14:44:50 +0100

    Delete contracts directory
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- contracts/mocks/DummyUserStatus.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract DummyUserStatus {
-  // Никто не бот, не VIP, не кит
-  function isFlaggedBot(address) external pure returns (bool) { return false; }
-  function isVIPUser(address) external pure returns (bool)    { return false; }
-  function isWhale(address) external pure returns (bool)      { return false; }
-}
