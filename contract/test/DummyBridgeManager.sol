e32984d51a50ac4334d9d6cff2a790dc4e66839d
VOVAN1980 <reker198079@gmail.com>
2025-12-08 22:33:42 +0100

    Delete contract directory
    Signed-off-by: VOVAN1980 <reker198079@gmail.com>

--- contract/test/DummyBridgeManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract DummyBridgeManager {
-    function isBridgeAddress(address) external pure returns (bool) {
-        return false;
-    }
-}
